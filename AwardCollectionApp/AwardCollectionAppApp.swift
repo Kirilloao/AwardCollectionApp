142470706b7cdf2b93443c11ce37fb6c66d20bd9
Kirilloao <kirilltaraturin747@gmail>
2023-08-31 13:19:20 +0200

    Initial Commit

---
+++ AwardCollectionApp/AwardCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardCollectionAppApp.swift
+//  AwardCollectionApp
+//
+//  Created by Kirill Taraturin on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwardCollectionApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AwardCollectionApp
+//
+//  Created by Kirill Taraturin on 31.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
